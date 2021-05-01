58573439e4b5665b26dac2ade6a937ca681795c4
Feligon <feligon07@gmail.com>
2021-05-01 01:10:21 -0300

    Início de aplicação das equações pelo compute shader.

---
+++ Objeto 4/Assets/Scripts/BaseCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCube : MonoBehaviour
+{
+    public float mass;
+    [SerializeField] ComputeShader cubeController = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeController.SetFloat("mass", mass);
+        cubeController.SetFloat("position", transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubeController.SetFloat("t", Time.time); 
+    }
+}
--- Objeto 4/Assets/Scripts/SimulationManager.cs
+++ Objeto 4/Assets/Scripts/SimulationManager.cs
@@ -14,6 +14,7 @@ public class SimulationManager : MonoBehaviour
     public float minMass;
     public float maxMass;
     public GameObject cube;
+    public GameObject cubeHolder;
     Vector3 startPosition;
 
     // Start is called before the first frame update
@@ -22,20 +23,18 @@ public class SimulationManager : MonoBehaviour
         startPosition = new Vector3(1,1,1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartSimulation()
     {
         objectCount = int.Parse(objectsInSimulationInput.text);
+        minMass = float.Parse(minMassInput.text);
+        maxMass = float.Parse(maxMassInput.text);
+
         for (int i = 0; i < objectCount; i++) 
         {
 
             float offsetX = -objectCount / 2 + i;
-            Instantiate(cube, new Vector3(offsetX*1.5f, startPosition.y, startPosition.z), Quaternion.identity);
+            cubeHolder = Instantiate(cube, new Vector3(offsetX*1.5f, startPosition.y, startPosition.z), Quaternion.identity);
+            cubeHolder.GetComponent<BaseCube>().mass = Random.Range(minMass, maxMass);
         }
         // Compute.mixMass = minMass;
         // Compute.maxMass = maxMass;
