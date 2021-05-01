d5fb2f7f53069c691be4574f81eb12a1c810513b
Douzane <guilherme.douzane@gmail.com>
2021-05-01 02:42:36 -0300

    Mudança de "caminho/base" para a centralização das funções no script "SimulationManager"
    Início do computeshader para movimentação.
    
    ToDo > Kernel(0) está retornando null, e as cores dos cubos ficam apenas pretas.

--- Objeto 4/Assets/Scripts/BaseCube.cs
+++ Objeto 4/Assets/Scripts/BaseCube.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class BaseCube : MonoBehaviour
 {
+
+    struct Cube {
+        public Vector3 position;
+        public Color color;
+        public float mass, speed, deltaT, t;
+    }
+
     public float mass;
     [SerializeField] ComputeShader cubeController = default;
+    public int iteractions;
 
     // Start is called before the first frame update
     void Start()
--- Objeto 4/Assets/Scripts/SimulationManager.cs
+++ Objeto 4/Assets/Scripts/SimulationManager.cs
@@ -5,17 +5,26 @@ using TMPro;
 
 public class SimulationManager : MonoBehaviour
 {
+    struct Cube
+    {
+        public Vector3 position;
+        public Color color;
+        public float mass, speed, deltaT, t;
+    }
+
     [SerializeField] TMP_InputField objectsInSimulationInput;
     [SerializeField] TMP_InputField minMassInput;
     [SerializeField] TMP_InputField maxMassInput;
-    
-
     public int objectCount;
     public float minMass;
     public float maxMass;
     public GameObject cube;
-    public GameObject cubeHolder;
+    public GameObject[] cubeHolders;
     Vector3 startPosition;
+    Cube[] cubeData;
+    [SerializeField] ComputeShader cubeController;
+    [SerializeField] int iteractions = 50;
+    private bool started = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,18 +34,35 @@ public class SimulationManager : MonoBehaviour
 
     public void StartSimulation()
     {
-        objectCount = int.Parse(objectsInSimulationInput.text);
-        minMass = float.Parse(minMassInput.text);
-        maxMass = float.Parse(maxMassInput.text);
-
-        for (int i = 0; i < objectCount; i++) 
+        if (started == false)
         {
+            objectCount = int.Parse(objectsInSimulationInput.text);
+            minMass = float.Parse(minMassInput.text);
+            maxMass = float.Parse(maxMassInput.text);
+            cubeData = new Cube[objectCount];
+            cubeHolders = new GameObject[objectCount];
 
-            float offsetX = -objectCount / 2 + i;
-            cubeHolder = Instantiate(cube, new Vector3(offsetX*1.5f, startPosition.y, startPosition.z), Quaternion.identity);
-            cubeHolder.GetComponent<BaseCube>().mass = Random.Range(minMass, maxMass);
+            for (int i = 0; i < objectCount; i++)
+            {
+                float offsetX = -objectCount / 2 + i;
+                cubeHolders[i] = Instantiate(cube, new Vector3(offsetX * 1.5f, startPosition.y, startPosition.z), Quaternion.identity);
+                cubeData[i].mass = Random.Range(minMass, maxMass);
+                cubeData[i].position = cubeHolders[i].transform.position;
+            }
+            started = true;
         }
-        // Compute.mixMass = minMass;
-        // Compute.maxMass = maxMass;
+        int totalsize = 4 * sizeof(float) + 3 * sizeof(float) + 3 * sizeof(float);
+        ComputeBuffer computeBuffer = new ComputeBuffer(cubeData.Length, totalsize);
+        computeBuffer.SetData(cubeData);
+        cubeController.SetBuffer(0, "cubes", computeBuffer);
+        cubeController.SetInt("iteractions", iteractions);
+        cubeController.Dispatch(0, cubeData.Length / 10, 1, 1);
+        computeBuffer.GetData(cubeData);
+        for (int i = 0; i < cubeHolders.Length; i++)
+        {
+            cubeHolders[i].GetComponent<MeshRenderer>().material.SetColor("_Color", cubeData[i].color);
+            cubeHolders[i].transform.position = cubeData[i].position;
+        }
+        computeBuffer.Dispose();
     }
 }
