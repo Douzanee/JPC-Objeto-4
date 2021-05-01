ba0794d0fe1312274b8a1b0b2d2207b76d12ddb2
Feligon <feligon07@gmail.com>
2021-05-01 17:19:57 -0300

    Voltar a calcular a velocidade de queda dos objetos a partir de equações.

--- Objeto 4/Assets/Scripts/SimulationManager.cs
+++ Objeto 4/Assets/Scripts/SimulationManager.cs
@@ -9,13 +9,14 @@ public class SimulationManager : MonoBehaviour
     {
         public Vector3 position;
         public Color color;
-        public float speed, mass;
+        public float speed, mass, v1, v2, dTime, time, t1, t2, force;
     }
 
     [SerializeField] TMP_InputField objectsInSimulationInput;
     [SerializeField] TMP_InputField minMassInput;
     [SerializeField] TMP_InputField maxMassInput;
     public int objectCount;
+    public int totalSize;
     public float minMass;
     public float maxMass;
     public GameObject cubeObj;
@@ -25,11 +26,12 @@ public class SimulationManager : MonoBehaviour
     [SerializeField] ComputeShader cubeController;
     [SerializeField] bool started = false;
     public Material material;
+    ComputeBuffer computeBuffer;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = new Vector3(1,1,1);
+        startPosition = Vector3.up * 100;
     }
 
     public void StartSimulation()
@@ -54,28 +56,36 @@ public class SimulationManager : MonoBehaviour
                 cubeHolders[i].GetComponent<MeshRenderer>().material = new Material(material);
                 cubeHolders[i].GetComponent<MeshRenderer>().material.SetColor("_Color", startColor);
                 cubeData[i].color = cubeHolders[i].GetComponent<MeshRenderer>().material.color;
-                cubeData[i].speed = 9.8f * Time.time;
+                //cubeData[i].speed = 9.8f * Time.deltaTime;
+                cubeData[i].t1 = Time.time;
+                cubeData[i].t2 = Time.time;
                 cubeData[i].position = cubeHolders[i].transform.position;
             }
+            int colorSize = sizeof(float) * 4;
+            int vector3Size = sizeof(float) * 3;
+            int variables = sizeof(float) * 9;
+            totalSize = colorSize + vector3Size + variables;
+
             started = true;
         }
     }
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            started = false;
+        }
         if (started)
         {
-            int colorSize = sizeof(float) * 4;
-            int vector3Size = sizeof(float) * 3;
-            int variables = sizeof(float) * 2;
-            int totalSize = colorSize + vector3Size + variables;
-            ComputeBuffer computeBuffer = new ComputeBuffer(cubeData.Length, totalSize);
-            //for (int i = 0; i < objectCount; i++)
-            //{
-            //    cubeData[i].position = cubeHolders[i].transform.position;
-            //}
+            
+            for (int i = 0; i < objectCount; i++)
+            {
+                cubeData[i].t2 = Time.time;
+            }
+            computeBuffer = new ComputeBuffer(cubeData.Length, totalSize);
             computeBuffer.SetData(cubeData);
             cubeController.SetBuffer(0, "cubes", computeBuffer);
-            cubeController.Dispatch(0, cubeData.Length / 11, cubeData.Length / 3, 1);
+            cubeController.Dispatch(0, cubeData.Length / 12, cubeData.Length / 3, 1);
             computeBuffer.GetData(cubeData);
             for (int i = 0; i < cubeHolders.Length; i++)
             {
