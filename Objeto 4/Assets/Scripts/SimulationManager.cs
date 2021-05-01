1120bd5466f406191d1e5cceeff7ed1f4d8b5814
Douzane <guilherme.douzane@gmail.com>
2021-04-30 22:43:35 -0300

    Adição do TextMeshPro
    Interface base para a simulação
    
    Script básico - não funcionando de spawn de objetos.

---
+++ Objeto 4/Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationManager : MonoBehaviour
+{
+    [SerializeField] InputField objectsInSimulationInput;
+    [SerializeField] InputField minMassInput;
+    [SerializeField] InputField maxMassInput;
+
+    public int objectCount;
+    public float minMass;
+    public float maxMass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartSimulation()
+    {
+        objectCount = objectsInSimulationInput.text.ToInt();
+
+        // Compute.mixMass = minMass;
+        // Compute.maxMass = maxMass;
+    }
+}
