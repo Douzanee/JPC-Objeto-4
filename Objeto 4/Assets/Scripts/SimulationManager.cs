3ca64f8bd1927942b4db34f42775ce7eb0a341f2
Douzane <guilherme.douzane@gmail.com>
2021-05-01 15:19:41 -0300

    Os cubos agora vão até a parte mais baixa da tela (pos 0) e ficam mudando de cor durante todo o percurso.

--- Objeto 4/Assets/Scripts/SimulationManager.cs
+++ Objeto 4/Assets/Scripts/SimulationManager.cs
@@ -9,7 +9,7 @@ public class SimulationManager : MonoBehaviour
     {
         public Vector3 position;
         public Color color;
-        public float mass, speed, deltaT, t;
+        public float speed, mass;
     }
 
     [SerializeField] TMP_InputField objectsInSimulationInput;
@@ -39,18 +39,18 @@ public class SimulationManager : MonoBehaviour
             objectCount = int.Parse(objectsInSimulationInput.text);
             minMass = float.Parse(minMassInput.text);
             maxMass = float.Parse(maxMassInput.text);
-            cubeData = new Cube[objectCount * objectCount];
+            cubeData = new Cube[objectCount];
             cubeHolders = new GameObject[objectCount];
 
             for (int i = 0; i < objectCount; i++)
             {
                 float offsetX = -objectCount / 2 + i;
-                cubeHolders[i] = Instantiate(cubeObj, new Vector3(offsetX * 1.5f, startPosition.y, startPosition.z), Quaternion.identity);
+                cubeHolders[i] = Instantiate(cubeObj, new Vector3(offsetX * 1.5f, startPosition.y + 10, startPosition.z), Quaternion.identity);
                 cubeData[i].mass = Random.Range(minMass, maxMass);
                 cubeData[i].position = cubeHolders[i].transform.position;
                 cubeHolders[i].GetComponent<MeshRenderer>().material = new Material(material);
                 cubeData[i].color = cubeHolders[i].GetComponent<MeshRenderer>().material.color;
-
+                cubeData[i].speed = 9.8f * Time.time;
             }
             started = true;
         }
@@ -61,7 +61,7 @@ public class SimulationManager : MonoBehaviour
         {
             int colorSize = sizeof(float) * 4;
             int vector3Size = sizeof(float) * 3;
-            int variables = sizeof(float) * 4;
+            int variables = sizeof(float) * 2;
             int totalSize = colorSize + vector3Size + variables;
             ComputeBuffer computeBuffer = new ComputeBuffer(cubeData.Length, totalSize);
             for (int i = 0; i < objectCount; i++)
@@ -70,13 +70,12 @@ public class SimulationManager : MonoBehaviour
             }
             computeBuffer.SetData(cubeData);
             cubeController.SetBuffer(0, "cubes", computeBuffer);
-            cubeController.SetFloat("t", Time.time);
-            cubeController.Dispatch(0, cubeData.Length / 4, 1, 1);
+            cubeController.Dispatch(0, cubeData.Length / 4, cubeData.Length / 8, 1);
             computeBuffer.GetData(cubeData);
             for (int i = 0; i < cubeHolders.Length; i++)
             {
                 cubeHolders[i].GetComponent<MeshRenderer>().material.SetColor("_Color", cubeData[i].color);
-                cubeHolders[i].transform.position = cubeData[i].position;
+                cubeHolders[i].transform.position -= new Vector3(0, cubeData[i].position.y, 0) * Time.deltaTime;
             }
             computeBuffer.Dispose();
 
