23d71a809e3b326a98182f10f16944b2ececb170
Feligon <feligon07@gmail.com>
2021-05-01 15:23:19 -0300

    Após considerável quantidade de comportamentos inesperados, foi decidido voltar a uma momento anterior do projeto para recomeçar, uma vez que esses comportamentos inesperados pudessem ter ocorrido por algum erro na base do projeto que ainda não havia sido identificado.

---
+++ Objeto 4/Assets/Scripts/BaseCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaseCube : MonoBehaviour
+{
+    public ComputeShader cubeController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Objeto 4/Assets/Scripts/SimulationManager.cs
+++ Objeto 4/Assets/Scripts/SimulationManager.cs
@@ -3,41 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
+public struct Cube
+{
+    public Vector3 position;
+    public Color color;
+}
+
 public class SimulationManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField objectsInSimulationInput;
     [SerializeField] TMP_InputField minMassInput;
     [SerializeField] TMP_InputField maxMassInput;
+    [SerializeField] ComputeShader cubeController;
     
 
     public int objectCount;
     public float minMass;
     public float maxMass;
-    public GameObject cube;
-    Vector3 startPosition;
+    public GameObject cubeGO;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        startPosition = new Vector3(1,1,1);
-    }
+    List<GameObject> cubes = new List<GameObject>();
+    Vector3 startPosition = Vector3.up;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Cube[] data;
 
     public void StartSimulation()
     {
         objectCount = int.Parse(objectsInSimulationInput.text);
+        data = new Cube[objectCount];
+
         for (int i = 0; i < objectCount; i++) 
         {
-
+            GameObject cube = new GameObject("Cube");
             float offsetX = -objectCount / 2 + i;
-            Instantiate(cube, new Vector3(offsetX*1.5f, startPosition.y, startPosition.z), Quaternion.identity);
+            cube.transform.position = new Vector3(offsetX, startPosition.y, startPosition.z);
+            cubes.Add(cube);
+
+            Cube cubeData = new Cube();
+            cubeData.position = cube.transform.position;
+            data[i] = cubeData;
         }
         // Compute.mixMass = minMass;
         // Compute.maxMass = maxMass;
+
+        int totalSize = sizeof(float) * 3 + sizeof(float) * 4;
+        ComputeBuffer buffer = new ComputeBuffer(data.Length, totalSize);
+        buffer.SetData(data);
+        cubeController.SetBuffer(0, "cubes", buffer);
+        cubeController.Dispatch(0, data.Length/10, 1, 1);
     }
 }
