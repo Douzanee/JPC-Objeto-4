88b0be9d4937ab3e5916cd37a2d579b4ed36edd4
Alberto Gregorio <quinho.saloes@gmail.com>
2021-04-30 23:55:33 -0300

    Spawn dos Cubos

--- Objeto 4/Assets/Scripts/SimulationManager.cs
+++ Objeto 4/Assets/Scripts/SimulationManager.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class SimulationManager : MonoBehaviour
 {
-    [SerializeField] InputField objectsInSimulationInput;
-    [SerializeField] InputField minMassInput;
-    [SerializeField] InputField maxMassInput;
+    [SerializeField] TMP_InputField objectsInSimulationInput;
+    [SerializeField] TMP_InputField minMassInput;
+    [SerializeField] TMP_InputField maxMassInput;
+    
 
     public int objectCount;
     public float minMass;
     public float maxMass;
+    public GameObject cube;
+    Vector3 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        startPosition = new Vector3(1,1,1);
     }
 
     // Update is called once per frame
@@ -26,8 +30,13 @@ public class SimulationManager : MonoBehaviour
 
     public void StartSimulation()
     {
-        objectCount = objectsInSimulationInput.text.ToInt();
+        objectCount = int.Parse(objectsInSimulationInput.text);
+        for (int i = 0; i < objectCount; i++) 
+        {
 
+            float offsetX = -objectCount / 2 + i;
+            Instantiate(cube, new Vector3(offsetX*1.5f, startPosition.y, startPosition.z), Quaternion.identity);
+        }
         // Compute.mixMass = minMass;
         // Compute.maxMass = maxMass;
     }
